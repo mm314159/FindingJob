fb99befa7182e6025723814e2ad9d58a51332d14
mm314159 <mm314159@gmx.com>
2015-12-01 21:27:46 -0500

    Test

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: [
+      'babel-polyfill',
+      "./src/entry",
+      'webpack/hot/dev-server',
+      'webpack-dev-server/client?http://localhost:8080'
+    ],
+    output: {
+        path: __dirname,
+        filename: "./public/bundle.js"
+    },
+    devServer: {
+      contentBase: "./src",
+      hot: true
+    },
+    module: {
+        loaders: [{
+              test: /\.jsx?$/, loader: 'babel',
+              include: path.join(__dirname, "src"),
+              loader: "babel-loader",
+              query: {
+                plugins: ['transform-runtime'],
+                presets: ['es2015', 'react'],
+              }
+            },
+              {
+                test: /\.scss$/,
+                include: path.join(__dirname, 'src'),
+                loader: 'style!css!sass'
+              }
+
+        ]
+    }
+};
+
+module.exports.config;
