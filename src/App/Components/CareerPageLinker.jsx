0bdf5dea70f5ca9a69a6caa0ae82f3289959cb1a
mm314159 <mm314159@gmx.com>
2015-12-06 20:40:26 -0500

    Tes
    Test

--- src/App/Components/CareerPageLinker.jsx
+++ src/App/Components/CareerPageLinker.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 
 
-var ProductCategoryRow = React.createClass({
+var ProductCategoryName = React.createClass({
     render: function() {
         return (<tr><th colSpan="1">{this.props.category}</th></tr>);
     }
@@ -25,7 +25,7 @@ var ProductTable = React.createClass({
         var lastCategory = null;
         this.props.linkers.forEach(function(product) {
             if (product.category !== lastCategory) {
-                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+                rows.push(<ProductCategoryName category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} />);
             lastCategory = product.category;
@@ -39,25 +39,12 @@ var ProductTable = React.createClass({
 });
 
 var Selection = React.createClass({
+    handleValue: function (){
+      alert:'hi';
+    },
   render: function() {
     return (
-      <form>
-          Consulting
-          <input
-            type="checkbox"
-            checked={this.props.sortment}
-            ref="checkOnly"
-            onChange={this.handleChange}
-           />
-          {''}
-          Insurance
-          <input type="checkbox" />
-          {''}
-          Health Care
-          <input type="checkbox" />
-          {''}
-          Federal Government
-      </form>
+      < input type = 'checkbox' value={this.props.industryType} name="industry" onClick={this.handleValue}/>
     );
   }
 });
@@ -66,15 +53,11 @@ var Selection = React.createClass({
 var FilterableProductTable = React.createClass({
     getInitialState: function() {
       return {
-        checkOnly: true
+
       };
     },
 
-    handleUserInput: function(checkOnly) {
-      this.setState({
-        checkOnly: checkOnly
-      });
-    },
+
 
 
     render: function() {
@@ -83,13 +66,9 @@ var FilterableProductTable = React.createClass({
               <Selection />
               <ProductTable
               linkers={this.props.linkers}
-              sortment={this.state.sortment}
-
-
-               />
-
+                             />
+              <Selection value= {'Consulting'  }/>
           </div>
-
         );
     }
 });
@@ -103,16 +82,20 @@ var linkers = [
   {category: 'Consulting', name: 'Deloitte', link: <a href="https://jobs2.deloitte.com/us/en/" target="_blank">Deloitte</a>},
   {category: 'Consulting', name: 'Booz Allen Hamilton', link: <a href="http://careers.boozallen.com/search/" target="_blank">Booz Allen Hamilton</a>},
   {category: 'Consulting', name: 'Accenture', link: <a href="https://www.accenture.com/us-en/careers/jobsearch" target="_blank">Accenture</a>},
-  {category: 'Accounting Firm', name: 'CliftonLarsonAllen', link: <a href="https://rn11.ultipro.com/CLI1000/JobBoard/SearchJobs.aspx?Page=Search">CliftonLarsonAllen</a>},
+  {category: 'Consulting', name: 'KPMG', link: <a href="http://us-jobs.kpmg.com/job-search/job-search-results/?" target="_blank">KPMG</a>},
+  {category: 'Accounting Firm', name: 'CliftonLarsonAllen', link: <a href="https://rn11.ultipro.com/CLI1000/JobBoard/SearchJobs.aspx?Page=Search" target="_blank">CliftonLarsonAllen</a>},
+  {category: 'Accounting Firm', name: 'BDO', link: <a href="https://www.bdo.com/careers" target="_blank">BDO</a>},
   {category: 'Insurance', name: 'Erie Insurance', link: <a href="https://careers-erieinsurance.icims.com/jobs/search?ss=1" target="_blank">Erie Insurance</a>},
+  {category: 'Insurance', name: 'NationWide', link: <a href="https://careers.nationwide.com/hcm/Nationwide-jobs-apply.html" target="_blank">NationWide</a>},
   {category: 'Insurance', name: 'Progressive', link: <a href="https://www.progressive.com/jobs/" target="_blank">Progressive</a>},
-  {category: 'Health Care', name: 'UPMC', link: <a href="http://careers.upmc.com/jobs/" target="_blank">UPMC</a>},
+  {category: 'Health Care', name: 'UPMC', link: <a href="http://careers.upmc.com/jobs/search" target="_blank">UPMC</a>},
   {category: 'Federal Government', name: 'Coporation of the United States of America', link: <a href="https://www.usajobs.gov/Search/GetResults?RecentGrad=Yes" target="_blank">USAJOBS</a>},
   {category: 'Manufacturer', name: 'Sherwin-Willaims', link: <a href="https://sherwin.taleo.net/careersection/10/moresearch.ftl?lang=en&portal=10100120119" target="_blank">Sherwin-Williams</a>},
   {category: 'Manufacturer', name: 'Bayer', link: <a href='https://www.career.bayer.com/en/career/job-search/' target="_blank">Bayer</a>},
-  {category: 'Civil Service', name: 'Pennsylvania-Civl', link: <a href="http://www.portal.state.pa.us/portal/server.pt/community/apply_now/17640/job_opportunities/613125" target="_blank">Pennsylvania</a>},
-  {category: 'Non-Civil Service', name: 'Pennsylvania-Non-Civil', link: <a href="https://secure.scsc.state.pa.us/OnlineServices/ui/OnlineLogin.aspx" target="_blank">Pennsylvania</a>},
+  {category: 'Non-Civil Service', name: 'Pennsylvania-Civl', link: <a href="http://www.portal.state.pa.us/portal/server.pt/community/apply_now/17640/job_opportunities/613125" target="_blank">Pennsylvania</a>},
+  {category: 'Civil Service', name: 'Pennsylvania-Non-Civil', link: <a href="https://secure.scsc.state.pa.us/OnlineServices/ui/OnlineLogin.aspx" target="_blank">Pennsylvania</a>},
   {category: 'Aerospace', name: 'Wyle', link: <a href="http://careers.wyle.com/Careers.aspx?adata=EGId5AstmCR1ug7qR5GktTiEwd7GosINPQtsbi1sVbpEjTD0idLEqPoT3%2bZDFQb0K6qvQ2SEYAcP16yzKoE2NnWMHy5umFthF%2furvNlmC4kolSu%2bd2w%2fpzPsAWMkp7CgkXJFNrWTdiHOXlQpNYtg8IwsqQP2nYIyqHrxSFAUpEoi" target="_blank">Wyle</a>},
+  {category: 'Retail', name: 'Walmart', link: <a href="https://jobs.walmart.com/us/jobs" target="_blank">Walmart</a>}
 ];
 
 ReactDOM.render(
--- src/App/Components/test.jsx
+++ src/App/Components/test.jsx
@@ -1,129 +1,118 @@
-var ProductCategoryRow = React.createClass({
-    render: function() {
-        return (<tr><th colSpan="2">{this.props.category}</th></tr>);
-    }
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function isDefined(val) { return val != null; }
+
+var ToggleDisplay = React.createClass({
+
+	propTypes: {
+		hide: React.PropTypes.bool,
+		show: React.PropTypes.bool
+	},
+
+	shouldHide: function() {
+		var shouldHide;
+		if(isDefined(this.props.show)) {
+			shouldHide = !this.props.show;
+		}
+		else if(isDefined(this.props.hide)) {
+			shouldHide = this.props.hide;
+		}
+		else {
+			shouldHide = false;
+		}
+
+		return shouldHide;
+	},
+
+	render: function() {
+		var style = {};
+
+		if(this.shouldHide()) {
+			style.display = 'none';
+		}
+
+		return (
+			<span style={style} {...this.props} />
+		);
+	}
+
 });
 
-var ProductRow = React.createClass({
-    render: function() {
-        var name = this.props.product.stocked ?
-            this.props.product.name :
-            <span style={{color: 'red'}}>
-                {this.props.product.name}
-            </span>;
-        return (
-            <tr>
-                <td>{name}</td>
-                <td>{this.props.product.price}</td>
-            </tr>
-        );
-    }
-});
+var App = React.createClass({
 
-var ProductTable = React.createClass({
-    render: function() {
-        var rows = [];
-        var lastCategory = null;
-        this.props.products.forEach(function(product) {
-            if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
-                return;
-            }
-            if (product.category !== lastCategory) {
-                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
-            }
-            rows.push(<ProductRow product={product} key={product.name} />);
-            lastCategory = product.category;
-        }.bind(this));
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
-        );
-    }
-});
-
-var SearchBar = React.createClass({
-    handleChange: function() {
-        this.props.onUserInput(
-            this.refs.filterTextInput.value,
-            this.refs.inStockOnlyInput.checked
-        );
-    },
-    render: function() {
-        return (
-            <form>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={this.props.filterText}
-                    ref="filterTextInput"
-                    onChange={this.handleChange}
-                />
-                <p>
-                    <input
-                        type="checkbox"
-                        checked={this.props.inStockOnly}
-                        ref="inStockOnlyInput"
-                        onChange={this.handleChange}
-                    />
-                    {' '}
-                    Only show products in stock
-                </p>
-            </form>
-        );
-    }
-});
-
-var FilterableProductTable = React.createClass({
-    render: function() {
+    getInitialState: function() {
         return {
-            filterText: '',
-            inStockOnly: false
+            show: true
+
         };
     },
 
-    handleUserInput: function(filterText, inStockOnly) {
-        this.setState({
-            filterText: filterText,
-            inStockOnly: inStockOnly
-        });
+    handleClick: function() {
+        this.setState({ show: !this.state.show });
     },
 
     render: function() {
         return (
             <div>
-                <SearchBar
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                    onUserInput={this.handleUserInput}
-                />
-                <ProductTable
-                    products={this.props.products}
-                    filterText={this.state.filterText}
-                    inStockOnly={this.state.inStockOnly}
-                />
+                <ToggleDisplay show={this.state.show}>
+                    <p>{consulting}</p>
+                </ToggleDisplay>
+                <ToggleDisplay hide={this.state.show}>
+                    <p>Is it me you're looking for?</p>
+                </ToggleDisplay>
+                <input type="checkbox" onClick={ this.handleClick }>Toggle</input>
             </div>
+
         );
     }
 });
 
+var consulting = [
+  {category: 'Consulting', name: 'PWC', link: <a href="http://jobs.pwc.com/" target="_blank">PWC</a>},
+  {category: 'Consulting', name: 'GrantThorton', link: <a href="http://jobs.grantthornton.com/jobs-most-recent.aspx" target="_blank">Grant Thorton</a>},
+  {category: 'Consulting', name: 'Boston Consulting Group', link: <a href="http://www.bcg.com/careers/join/apply-bst/default.aspx" target="_blank">Boston Consulting Group</a>},
+  {category: 'Consulting', name: 'McKinsey & Company', link: <a href="http://www.mckinsey.com/careers/join_us/search_and_apply" target="_blank">McKinsey & Company</a>},
+  {category: 'Consulting', name: 'Deloitte', link: <a href="https://jobs2.deloitte.com/us/en/" target="_blank">Deloitte</a>},
+  {category: 'Consulting', name: 'Booz Allen Hamilton', link: <a href="http://careers.boozallen.com/search/" target="_blank">Booz Allen Hamilton</a>},
+  {category: 'Consulting', name: 'Accenture', link: <a href="https://www.accenture.com/us-en/careers/jobsearch" target="_blank">Accenture</a>},
+  {category: 'Consulting', name: 'KPMG', link: <a href="http://us-jobs.kpmg.com/job-search/job-search-results/?" target="_blank">KPMG</a>},
+],
 
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
+var AccountingFirm = [
+  {category: 'Accounting Firm', name: 'CliftonLarsonAllen', link: <a href="https://rn11.ultipro.com/CLI1000/JobBoard/SearchJobs.aspx?Page=Search" target="_blank">CliftonLarsonAllen</a>},
+  {category: 'Accounting Firm', name: 'BDO', link: <a href="https://www.bdo.com/careers" target="_blank">BDO</a>},
+],
 
-ReactDOM.render(
-    <FilterableProductTable products={PRODUCTS} />,
-    document.getElementById('container')
-);
+var Insurance = [
+  {category: 'Insurance', name: 'Erie Insurance', link: <a href="https://careers-erieinsurance.icims.com/jobs/search?ss=1" target="_blank">Erie Insurance</a>},
+  {category: 'Insurance', name: 'NationWide', link: <a href="https://careers.nationwide.com/hcm/Nationwide-jobs-apply.html" target="_blank">NationWide</a>},
+  {category: 'Insurance', name: 'Progressive', link: <a href="https://www.progressive.com/jobs/" target="_blank">Progressive</a>},
+],
+
+var HealthCare = [
+  {category: 'Health Care', name: 'UPMC', link: <a href="http://careers.upmc.com/jobs/search" target="_blank">UPMC</a>},
+],
+
+var FederalGovernment = [
+  {category: 'Federal Government', name: 'Coporation of the United States of America', link: <a href="https://www.usajobs.gov/Search/GetResults?RecentGrad=Yes" target="_blank">USAJOBS</a>},
+],
+
+var Manufactuere = [
+  {category: 'Manufacturer', name: 'Sherwin-Willaims', link: <a href="https://sherwin.taleo.net/careersection/10/moresearch.ftl?lang=en&portal=10100120119" target="_blank">Sherwin-Williams</a>},
+  {category: 'Manufacturer', name: 'Bayer', link: <a href='https://www.career.bayer.com/en/career/job-search/' target="_blank">Bayer</a>},
+],
+
+var NonCivilService = [
+  {category: 'Non-Civil Service', name: 'Pennsylvania-Civl', link: <a href="http://www.portal.state.pa.us/portal/server.pt/community/apply_now/17640/job_opportunities/613125" target="_blank">Pennsylvania</a>},
+],
+  {category: 'Civil Service', name: 'Pennsylvania-Non-Civil', link: <a href="https://secure.scsc.state.pa.us/OnlineServices/ui/OnlineLogin.aspx" target="_blank">Pennsylvania</a>},
+  {category: 'Aerospace', name: 'Wyle', link: <a href="http://careers.wyle.com/Careers.aspx?adata=EGId5AstmCR1ug7qR5GktTiEwd7GosINPQtsbi1sVbpEjTD0idLEqPoT3%2bZDFQb0K6qvQ2SEYAcP16yzKoE2NnWMHy5umFthF%2furvNlmC4kolSu%2bd2w%2fpzPsAWMkp7CgkXJFNrWTdiHOXlQpNYtg8IwsqQP2nYIyqHrxSFAUpEoi" target="_blank">Wyle</a>},
+  {category: 'Retail', name: 'Walmart', link: <a href="https://jobs.walmart.com/us/jobs" target="_blank">Walmart</a>}
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('container'));
